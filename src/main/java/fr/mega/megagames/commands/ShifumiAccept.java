9eb96d66c149a25a2366f771b65afd58457d20d4
Mega02600 <haouasjeremy@gmail.com>
2024-07-30 14:08:49 +0200

    .

--- src/main/java/fr/mega/megagames/commands/ShifumiAccept.java
+++ src/main/java/fr/mega/megagames/commands/ShifumiAccept.java
@@ -1,15 +1,31 @@
 package fr.mega.megagames.commands;
 
+import com.google.common.escape.ArrayBasedUnicodeEscaper;
 import fr.mega.megagames.ItemBuilder;
+import fr.mega.megagames.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class ShifumiAccept implements CommandExecutor{
+
+    HashMap<UUID, UUID> Hashmap = new HashMap<>();
+    private int Time = 10;
+    private boolean SenderClick = false;
+    private boolean FirstChooseIsStone = false;
+    private boolean FirstChooseIsPaper = false;
+    private boolean FirstChooseIsCisor = false;
+
 
     @Override
     public boolean onCommand(CommandSender Sender, Command msg, String s, String[] strings) {
@@ -21,6 +37,7 @@ public class ShifumiAccept implements CommandExecutor{
                     player.sendMessage(Sender.getName() + "viens d'§aaccepter §rvotre demande de duel");
                     player.openInventory(Shifumi(player));
                     ((Player) Sender).openInventory(Shifumi((Player) Sender));
+                    Hashmap.put(((Player) Sender).getUniqueId(), player.getUniqueId());
                 }
             }
         }
@@ -34,6 +51,20 @@ public class ShifumiAccept implements CommandExecutor{
         inv.setItem(7, new ItemBuilder(Material.SHEARS).setName("§cCiseau").toItemStack());
 
         return inv;
+    }
+    @EventHandler
+    public void OnInventoryClick(InventoryClickEvent event){
+        if(event.getClickedInventory().getTitle().equals("Shifumi")){
+            new BukkitRunnable(){
+
+                @Override
+                public void run() {
+                    if (!SenderClick && Time >= 0){
+                        Time --;
+                    }
+                }
+            }.runTaskTimer(Main.getPlugin(Main.class), 0,20);
+        }
     }
 
 
